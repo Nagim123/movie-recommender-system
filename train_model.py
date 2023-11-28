12504e6139735fae3ea2b5315920200f25073dd5
Nagim <39875672+Nagim123@users.noreply.github.com>
2023-11-28 17:50:47 +0300

    Add command line arguments

--- train_model.py
+++ train_model.py
@@ -8,12 +8,13 @@ import tqdm
 import os
 import argparse
 
-def create_dataloader(data: HeteroData) -> LinkNeighborLoader:
+def create_dataloader(data: HeteroData, batch_size: int) -> LinkNeighborLoader:
     """
     Create dataloader from graph.
 
     Paramaters:
         data (HeteroData): graph.
+        batch_size (int): Size of a batch.
     
     Returns:
         LinkNeighborLoader: data loader.
@@ -23,7 +24,7 @@ def create_dataloader(data: HeteroData) -> LinkNeighborLoader:
         # Sample 128 neighbors for each node for 2 iterations
         num_neighbors=[128] * 2,
         # Use a batch size of 256 for sampling training nodes
-        batch_size=256,
+        batch_size=batch_size,
         edge_label_index=(("user", "rates", "movie"), data["user", "rates", "movie"].edge_label_index),
         edge_label=data["user", "rates", "movie"].edge_label
     )
@@ -70,7 +71,6 @@ def val_one_epoch(model: RecommendationModel, train_data: HeteroData, val_data: 
     """
     model.eval()
     with torch.no_grad():
-        total_loss = 0
         # Get predictions from model and true labels
         embeds = model.encode_graph(train_data)
         edges = val_data["user", "rates", "movie"].edge_label_index
@@ -81,10 +81,16 @@ def val_one_epoch(model: RecommendationModel, train_data: HeteroData, val_data: 
     return loss.item()
 
 if __name__ == "__main__":
-    train_data = HeteroData(_mapping=torch.load(os.path.join(INTERIM_PATH, "data1_train.pt")))
-    val_data = HeteroData(_mapping=torch.load(os.path.join(INTERIM_PATH, "data1_val.pt")))
-    print(train_data)
-    train_loader = create_dataloader(train_data)
+    parser = argparse.ArgumentParser(description="GNN training script. Specify part of dataset to start training.")
+    parser.add_argument("part", choices=['1', '2', '3', '4', '5', 'a', 'b'])
+    parser.add_argument("--epochs", type=int, default=10)
+    parser.add_argument("--batch_size", type=int, default=256)
+    args = parser.parse_args()
+
+
+    train_data = HeteroData(_mapping=torch.load(os.path.join(INTERIM_PATH, f"data{args.part}_train.pt")))
+    val_data = HeteroData(_mapping=torch.load(os.path.join(INTERIM_PATH, f"data{args.part}_val.pt")))
+    train_loader = create_dataloader(train_data, args.batch_size)
     
     # Set device
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -93,11 +99,11 @@ if __name__ == "__main__":
     # Use Adam optmizer
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
     loss_fn = nn.MSELoss()
-    epochs = 10
+    epochs = args.epochs
     best_loss = 1e9
 
     for epoch in range(epochs):
-        train_loss = train_one_epoch(model, train_loader, loss_fn, optimizer, device)
+        train_loss = train_one_epoch(model, train_loader, optimizer, loss_fn, device)
         val_loss = val_one_epoch(model, train_data, val_data, loss_fn, device)
         if val_loss < best_loss: # Saving best model
             best_loss = val_loss
